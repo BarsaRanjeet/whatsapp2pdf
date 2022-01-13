77b5e555319dfc96aa06f9ee211483deb4774ed4
Ranjit Barsa <ranjit@atcults.com>
2022-01-13 17:14:37 +0530

    commit changes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import * as fs from 'fs'
 import prompt from 'prompt';
 import moment from 'moment';
 import { GeneratePDF } from './pdf.mjs'
-import { mapAsync } from 'lodasync'
 
 const data = {
     FromDate: new Date("2022-01-08"),
-    ToDate: new Date("2022-01-10T23:59:59.999Z"),
+    ToDate: new Date("2022-01-11T23:59:59.999Z"),
     start_id: null,
     end_id: null,
     cursor: null,
-    count: 0
+    count: 0,
+    cursorCount: 10
 }
 
-const office_order = "919909955422-1465194455@g.us";
+const test = "918866464813-1602851606@g.us";
+let office_order = "919909955422-1465194455@g.us";
+
+// replacing for development
+office_order = test
 
 /***
  * Getting inputs from user such as from date and todate
@@ -80,7 +84,7 @@ conn.on('chat-update', chatUpdate => {
         // console.log(message.key.id)
         // const contact = conn.chats.get(chatUpdate.jid);
         // console.log(contact);
-        // console.log(message);
+        console.log(message);
     }
     else console.log(chatUpdate);
 })
@@ -94,7 +98,7 @@ function storeSession(conn) {
 const loadMessages = (cursor) => {
     return new Promise(async (resolutionFunc) => {
         // const chats = await conn.loadMessages("918866464813-1602851606@g.us", 10, cursor); // Note
-        const chats = await conn.loadMessages(office_order, 10, cursor); // Note
+        const chats = await conn.loadMessages(office_order, data.cursorCount, cursor); // Note
         resolutionFunc(chats);
     });
 }
@@ -102,19 +106,28 @@ const loadMessages = (cursor) => {
 // after getting From and To date messages will get final messages
 const loadFinalMessages = (cursor, count) => {
     return new Promise(async (resolutionFunc) => {
-        const chats = await conn.loadMessages(office_order, count, cursor); // Note
+        const chats = await conn.loadMessages(office_order, (count == 0) ? data.cursorCount : count, cursor); // Note
         resolutionFunc(chats);
     });
 }
 
 
 const findDate = () => {
+    const startDate = moment(data.FromDate).format("YYYY-MM-DD");
+    var given = moment(startDate, "YYYY-MM-DD");
+    var current = moment().startOf('day');
+
+    //Difference in number of days
+    const diff = moment.duration(current.diff(given)).asDays();
+    if (diff > 5)
+        data.cursorCount = diff * 3;
+
     loadMessages(data.cursor).then((val) => {
         const messages = val.messages;
         const past_day = messages[0].messageTimestamp.low * 1000 + (5.5 * 60 * 60 * 1000);
         // const last_day = messages[messages.length - 1].messageTimestamp.low * 1000 + (5.5 * 60 * 60 * 1000); // incase if required this variable
 
-        // console.log(new Date(messages[0].messageTimestamp.low * 1000 + (5.5 * 60 * 60 * 1000)));
+        console.log(new Date(messages[0].messageTimestamp.low * 1000 + (5.5 * 60 * 60 * 1000)));
 
         // if both FromDate and ToDate are not worthy
         if (past_day >= data.FromDate.getTime() && past_day >= data.ToDate.getTime()) {
@@ -126,7 +139,7 @@ const findDate = () => {
         // if from date is worthy
         if (past_day <= data.FromDate.getTime()) {
             data.start_id = val.cursor;
-            if (data.end_id != null) data.count += 10;
+            data.count = (data.end_id != null) ? data.count + data.cursorCount : data.count;
         }
 
         // if To Date is worthy
@@ -137,51 +150,42 @@ const findDate = () => {
         // If both not satisfied then will be trying finding correct date
         if (data.start_id == null || data.end_id == null) {
             data.cursor = val.cursor;
-            if (data.end_id != null) data.count += 10;
+            data.count = (data.end_id != null) ? data.count + data.cursorCount : data.count;
             findDate();
             return
+        }
+
+        const messageTyeps = {
+            imageMessage: "Image",
+
         }
 
         // load all messages between from date to To date
         loadFinalMessages(data.end_id, data.count).then(async (val) => {
             const finalMessages = val.messages;
             const results = [];
-            // const result = finalMessages.map(async (msg) => {
-            //     const msg_timestamp = msg.messageTimestamp.low * 1000; // for local + (5.5 * 60 * 60 * 1000)
-            //     if (data.ToDate >= msg_timestamp && data.FromDate <= msg_timestamp) {
-            //         const message_data = msg.message;
-            //         const key = msg.key;
-            //         const from = conn.contacts[msg.participant].notify;
-            //         const date = moment(new Date(msg_timestamp)).format('MMM Do YYYY, h:mm a');
-            //         const image = (message_data.imageMessage) ? await conn.downloadAndSaveMediaMessage(msg, `./src/images/${key.id}`) : "";
-            //         const message = (message_data.conversation) ? message_data.conversation : "";
-            //         const type = "";
-            //         return { date, type, from, message, image };
-            //     }
-            // })
-            const result = await mapAsync(async (msg) => {
+            for (let msg of finalMessages) {
                 const msg_timestamp = msg.messageTimestamp.low * 1000; // for local + (5.5 * 60 * 60 * 1000)
-                if (data.ToDate >= msg_timestamp && data.FromDate <= msg_timestamp) {
+                if (data.ToDate >= msg_timestamp && data.FromDate <= msg_timestamp && msg.message != null) {
                     const message_data = msg.message;
                     const key = msg.key;
-                    const from = conn.contacts[msg.participant].notify;
+                    // const from = conn.contacts[msg.participant].notify;
+                    const from = conn.contacts[msg.participant];
                     const date = moment(new Date(msg_timestamp)).format('MMM Do YYYY, h:mm a');
                     const image = (message_data.imageMessage) ? await conn.downloadAndSaveMediaMessage(msg, `./src/images/${key.id}`) : "";
                     const message = (message_data.conversation) ? message_data.conversation : "";
                     const type = "";
                     results.push({ date, type, from, message, image });
                 }
-            }, finalMessages)
+            }
 
-            Promise.all(result).then(() => {
-                const genPDF = new GeneratePDF(results);
-                genPDF.generate();
-                console.log("Done!!");
-            })
+            const genPDF = new GeneratePDF(results);
+            genPDF.generate();
+            console.log("Done!!");
 
         });
     });
 };
 
 findDate();
-// });
+// });  
